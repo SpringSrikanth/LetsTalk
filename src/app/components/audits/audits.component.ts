b6bd341e58f372e22843d0e9983f4d8a2dac434a
SpringSrikanth <rajudusa59@gmail.com>
2022-04-02 19:32:28 +0530

    added minor changes in audit table

--- src/app/components/audits/audits.component.ts
+++ src/app/components/audits/audits.component.ts
@@ -13,7 +13,7 @@ export class AuditsComponent implements OnInit {
   displayedColumns=["email","client_ip","server_ip","client_region","uri","statusCode","client_org","server_org","client_timezone","server_timezone","createdAt"];
   pageSize = 10;
   pageIndex=0;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageSizeOptions: number[] = [5, 10,15,20, 25,50,100];
 
   constructor(private service:AuditsService) { }
 
@@ -22,15 +22,13 @@ export class AuditsComponent implements OnInit {
   }
   loadAudits(pageIndex,pageSize){
     this.service.getAuditsByPagination(pageIndex,pageSize).subscribe((res)=>{
-      console.log(res);
-      
+      // console.log(res);
       this.auditData=res;
     })
 
   }
   handlePage(event){
-    console.log(event);
-
+    // console.log(event);
     this.loadAudits(event.pageIndex,event.pageSize)
   }
 
