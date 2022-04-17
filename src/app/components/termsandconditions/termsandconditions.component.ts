ab7dbec2fbec46626a93e931352d9539fe345beb
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-04-17 19:14:58 +0530

    Added changes for pdf load

--- src/app/components/termsandconditions/termsandconditions.component.ts
+++ src/app/components/termsandconditions/termsandconditions.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { PDFSource, PdfViewerComponent } from 'ng2-pdf-viewer';
 @Component({
   selector: 'app-termsandconditions',
   templateUrl: './termsandconditions.component.html',
@@ -8,11 +9,37 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class TermsandconditionsComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<TermsandconditionsComponent>) { }
-
+  pdfSrc: string | PDFSource | ArrayBuffer = './assets/pdf-test.pdf';
   ngOnInit(): void {
+    this.loadPdf();
   }
 
-  closeDialog(){
+  closeDialog() {
     this.dialogRef.close();
+  }
+
+  get zoom(){
+    return window.screen.width <= 768 ? 2  : 1 
+  }
+
+  loadPdf() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/assets/test.pdf', true);
+    xhr.responseType = 'blob';
+    xhr.onload = (e: any) => {
+      if (xhr.status === 200) {
+        const blob = new Blob([xhr.response], { type: 'application/pdf' });
+        this.pdfSrc = URL.createObjectURL(blob);
+      }
+    };
+    xhr.send();
+  }
+
+  @ViewChild(PdfViewerComponent) private pdfComponent: PdfViewerComponent;
+
+  search(stringToSearch: string) {
+    this.pdfComponent.pdfFindController.executeCommand('find', {
+      caseSensitive: false, findPrevious: undefined, highlightAll: true, phraseSearch: true, query: stringToSearch
+    });
   }
 }
